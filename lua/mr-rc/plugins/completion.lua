10a71db936104f9a82be75eb466ce7aeb13c0d95
codr <mr-codr@proton.me>
2024-03-13 21:08:09 -0300

    feat: toggle completion with `<C-e>`

--- lua/mr-rc/plugins/completion.lua
+++ lua/mr-rc/plugins/completion.lua
@@ -1,51 +1,48 @@
 return {
-  "hrsh7th/nvim-cmp", -- completion engine
-  event = 'InsertEnter',
-  dependencies = {
-    'hrsh7th/cmp-path',
-    "L3MON4D3/LuaSnip", -- Snippets engine
-  },
-  config = function ()
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip/loaders/from_vscode").lazy_load()
+	"hrsh7th/nvim-cmp", -- completion engine
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip", -- Snippets engine
+	},
+	config = function()
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+		require("luasnip/loaders/from_vscode").lazy_load()
 
-    local cmp = require('cmp');
-    cmp.setup {
-      completion = {
-        completeopt = 'menu,menuone,preview,noselect',
-      },
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-1),
-        ['<C-f>'] = cmp.mapping.scroll_docs(1),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      }),
-
-      formatting = {
-        fields = { "abbr", "kind" },
-        -- format = function(_, vim_item)
-        --   vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-        --   return vim_item
-        -- end
-      },
-
-      sources = cmp.config.sources {
-        { name = "nvim_lsp" },
-        { name = 'path' },
-      },
-
-      window = {
-        documentation = cmp.config.window.bordered(),
-      },
-    }
-  end
-
+		local cmp = require("cmp")
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			formatting = {
+				fields = { "abbr", "kind" },
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<C-b>"] = cmp.mapping.scroll_docs(-1),
+				["<C-n>"] = cmp.mapping.scroll_docs(1),
+				["<C-e>"] = function()
+					if cmp.visible() then
+						cmp.abort()
+					else
+						cmp.complete()
+					end
+				end,
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+			}),
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				documentation = cmp.config.window.bordered(),
+			},
+		})
+	end,
 }
