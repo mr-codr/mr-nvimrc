481a8ac5238ad344d360859f49ed21513ffb40cb
codr <mr-codr@proton.me>
2024-03-16 11:48:31 -0300

    feat: improve buffer deletion plugin

---
+++ lua/mr-rc/plugins/bufremove.lua
@@ -0,0 +1,8 @@
+return {
+	"echasnovski/mini.bufremove",
+	version = false,
+	config = true,
+	keys = {
+		{ "<leader>kk", "<CMD>lua MiniBufremove.delete()<CR>" },
+	},
+}
--- lua/mr-rc/plugins/close-buffers.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"Asheq/close-buffers.vim",
-	keys = {
-		{ "<leader>kk", "<cmd>Bdelete this<cr>" },
-		{ "<leader>ko", "<cmd>Bdelete hidden<cr>" },
-	},
-}
