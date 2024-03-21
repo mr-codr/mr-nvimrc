ea7375532798ffd6e586d6043a8b42fc4636e262
codr <mr-codr@proton.me>
2024-03-21 08:51:12 -0300

    feat: remove `lsp` folder

--- lua/mr-rc/init.lua
+++ lua/mr-rc/init.lua
@@ -1,27 +1,26 @@
-require "mr-rc.keymaps"
-require "mr-rc.options"
+require("mr-rc.keymaps")
+require("mr-rc.options")
+require("mr-rc.features")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{ import = 'mr-rc.plugins' }, { import = 'mr-rc.plugins.lsp' }},
-  {
-    defaults = { lazy = true },
-    -- install = { colorscheme = { 'tokyonight-night' } },
-    checker = {
-      enabled = true,
-      notify = false,
-    },
-    change_detection = { notify = false },
-  }
-)
+require("lazy").setup("mr-rc.plugins", {
+	defaults = { lazy = true },
+	-- install = { colorscheme = { 'tokyonight-night' } },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = { notify = false },
+})
